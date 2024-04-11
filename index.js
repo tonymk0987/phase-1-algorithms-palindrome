1bda91a57c616db8ff700dc5f19785f532844093
tonymk0987 <k.tony.muriuki@gmail.com>
2024-04-11 19:06:09 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
-
+function reverseString(str) {
+  // Converting  the string to an array, reversing it and then joininig it back into a string
+  return str.split('').reverse().join('');
+}
 /* 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+
+ The REVERSE function is used to reverse the input word. 
+ This function takes the input string and returns a new string with its characters in reverse order.
+
+Comparison: The original word and the reversed word are compared using the equality operator (===). 
+If the original word is the same as the reversed word, it means that the word reads the same forwards and backwards, 
+indicating that it's a palindrome.
+
+Return: The function returns true if the original word is equal to the reversed word, 
+indicating that it's a palindrome. Otherwise, it returns false.
+
+
 */
 
 // You can run `node index.js` to view these console logs
